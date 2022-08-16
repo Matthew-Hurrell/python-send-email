15949a5c76941c1ab9046a8c6e64be113275f145
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2022-08-16 09:26:42 +0000

    Add basic create playlist function to linked account

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+
+SPOTIFY_CREATE_PLAYLIST_URL = 'https://api.spotify.com/v1/users/1196941328/playlists'
+ACCESS_TOKEN = 'BQC7DPGv4-wOB5THxPS0ejeAX7AX6_Ien1IvEgCr396s4hNjNfJlXfu_DULoQIuq1iVVqR2BhwjN8Ppcoi-toLl-8lUMYAZbe8czT1019rvOyPB9HCJiV79EVPkZANEL3OMlXaWEerSU1eZOOSfcLwsXrVlxLgL2EZFwLfaiQpFLQlUxHiaWTlQvphrxHiQcZBvEIeyf1-pPDXKYTKFyHzv03TVgzCvVax8AOBGvTbdc'
+
+def create_playlist(name, public):
+    response = requests.post(
+        SPOTIFY_CREATE_PLAYLIST_URL,
+        headers={
+            "Authorization": f"Bearer {ACCESS_TOKEN}"
+        },
+        json={
+            "name": name,
+            "public": public
+        }
+    )
+    json_resp = response.json()
+
+    return json_resp
+
+def main():
+    playlist = create_playlist(
+        name="My Private Playlist",
+        public=False
+    )
+
+    print(f"Playlist: {playlist}")
+
+if __name__ == '__main__':
+    main()
